3f6c99f0a516131f8e86d0a1854972273ec04f16
Yohan <yohtm@hotmail.com>
2014-11-10 14:52:24 -0500

    testing with real harware. thread-safe, better string manipulations and flashing

--- scripts/comm/communication.py
+++ scripts/comm/communication.py
@@ -2,6 +2,7 @@ import string
 import sys
 import traceback
 import rospy
+import threading
 
 import re
 from serial_com import SerialCom
@@ -22,6 +23,7 @@ class Communication:
         self.baudrate = baudrate
         self.readTimeout = readTimeout
         self.serial  = SerialComInstance
+        self.lock = threading.Lock()
 
     def start(self):
                 
@@ -35,62 +37,73 @@ class Communication:
         rospy.loginfo( "communication: Closing serial connection.")
         self.serial.close()
 
-
     def send_command(self, command_to_send):
-        p1 = re.compile("[SETset]+ [0-9a-zA-Z]+ [ONFonf]+")
-        p2 = re.compile("[GETget]+ [0-9a-zA-Z]+")
-        
-        if p1.match(command_to_send) is None and p2.match(command_to_send) is None:
-            rospy.logerr("communication: Invalid command")
-            return False, self.help()
 
-        parts = string.split(command_to_send, " ")
+        result = None, None
 
-        command = parts[0].upper()
-        deviceId = self.get_sensor_addr(parts[1])
-        if deviceId == None:
-            return False, "Error: Device not in dictionnary"
+        self.lock.acquire()
+        try:
+            p1 = re.compile("[SETset]+ [0-9a-zA-Z]+ [ONFonf]+")
+            p2 = re.compile("[GETget]+ [0-9a-zA-Z]+")
 
-        if len(parts) > 2:
-            state = parts[2]
+            if p1.match(command_to_send) is None and p2.match(command_to_send) is None:
+                rospy.logerr("communication: Invalid command")
+                result = False, self.help()
 
-        #SET stuff
-        if command == "SET":
-            try:
+            parts = string.split(command_to_send, " ")
+
+            command = parts[0].upper()
+            deviceId = self.get_sensor_addr(parts[1])
+            if deviceId == None:
+                result = False, "Error: Device not in dictionnary"
+
+            if len(parts) > 2:
+                state = parts[2]
+
+            #SET stuff
+            if command == "SET":
                 try:
-                    self.serial.write(command + " " + str(deviceId) + " " + state.upper() + "\n")
+                    try:
+                        self.serial.write(command + " " + str(deviceId) + " " + state.upper() + "\n")
+                        rospy.sleep(0.1)
+                    except:
+                        rospy.logerr( "communication: error writing to serial port")
+                        result = False, None
+
+                    return True, None
+                except:
+                    traceback.print_exc(file=sys.stdout)
+                    result = False, self.help()
+
+            #GET stuff
+            if command == "GET":
+                try:
+                    self.serial.write(command + " " + str(deviceId) + " " + "\n")
                 except:
                     rospy.logerr( "communication: error writing to serial port")
-                    return False, None
-                
-                return True, None
-            except:
-                traceback.print_exc(file=sys.stdout)
-                return False, self.help()
-        
-        #GET stuff
-        if command == "GET":
-            try:
-                self.serial.write(command + " " + str(deviceId) + " " + "\n")
-            except:
-                rospy.logerr( "communication: error writing to serial port")
-                return False, None
-            
-            try:
-                reply = self.serial.read(1024)
-            except:
-                traceback.print_exc(file=sys.stdout)
-                return False, self.help()
-        
-            if reply == None:
-                rospy.logerr( "Communication: Invalid command")
-                return False, None
-            elif reply == "":
-                rospy.logerr( "Communication: WARNING - Empty response from hardware")
-            else:
-                reply = command_to_send.split(" ")[1] + " " + reply
+                    result = False, None
 
-            return True, False
+                try:
+                    reply = self.serial.read(1024)
+                    reply = reply.replace("\n", "")
+                except:
+                    traceback.print_exc(file=sys.stdout)
+                    result = False, self.help()
+
+                if reply == None:
+                    rospy.logerr( "Communication: Invalid command")
+                    result = False, None
+                elif reply == "":
+                    rospy.logerr( "Communication: WARNING - Empty response from hardware")
+                    result = False, None
+                else:
+                    result = True, command_to_send.split(" ")[1] + " " + reply
+
+                rospy.logdebug("Controlpanel/communication replying: " + reply)
+
+        finally:
+            self.lock.release()
+            return result
 
     def get_sensor_addr(self, name):
         for var, value in self.dict.items():
@@ -112,17 +125,3 @@ class Communication:
         helpMessage += "  " + "\n  ".join(self.get_all_sensors())
         helpMessage += "\n"
         return helpMessage
-
-    def checkSum(self, hexArray):
-        # compute checksum from array
-        checksum = 0x00
-        for e in hexArray:
-            if e == None:
-                continue
-            checksum += e
-
-        # limit checksum
-        while checksum > 0xFF:
-            checksum -= 0xFF
-
-        return checksum
--- scripts/controlpanel_node.py
+++ scripts/controlpanel_node.py
@@ -1,43 +1,84 @@
 #!/usr/bin/env python
 
 import rospy
+import threading
 from capra_controlpanel.msg import *
 from capra_controlpanel.srv import *
 
 import comm.communication
 from comm.communication import Communication
 
-import time
+flash_state = False
 
 def handle_controlpanel_set(req):
-    rospy.logdebug("got call SET with param " + req.name + " and " + str(req.state))
-    
+    rospy.loginfo("got call SET with param " + req.name + " and " + str(req.state))
     cmd = "ON"
     if req.state is False:
         cmd = "OFF"
-    
-    status, reply = comm.communication.instance.send_command("SET " + req.name + " " + cmd)
-    
+
+    send("SET " + req.name + " " + cmd)
+
+    return True
+
+def handle_controlpanel_flash(req):
+    global flash_state
+
+    rospy.loginfo("got call FLASH with param " + str(req.state))
+
+    if req.state is True and  flash_state is False:
+        flash_state = True
+        flash_thread = threading.Thread(target = flash, args = (rospy.get_param('~flash_rate'),))
+        flash_thread.start()
+
+    if req.state is False and flash_state is True:
+        flash_state = False
+
+    return True
+
+def flash(rate):
+    global flash_state
+
+    light_on = True
+
+    while flash_state:
+
+        if light_on is True:
+            light_on = False
+            send("SET LIGHTS OFF")
+        else:
+            light_on = True
+            send("SET LIGHTS ON")
+
+        sleep_time = 1.0/rate
+        rospy.sleep(sleep_time)
+
+def send(cmd):
+
+    status, reply = comm.communication.instance.send_command(cmd)
+
     if not status:
         reply = "communication error"
-    
+
     if reply is None:
         reply = ""
     else:
         rospy.logdebug("handle_controlpanel_set: sending response to client: '" + reply + "'")
 
-    return True
-    
+    return reply
+
+
 class ControlPanelServer:
 
     def __init__(self):
+
         rospy.init_node('capra_controlpanel')
-        rospy.set_param
+
         pub_robot_buttons = rospy.Publisher("~buttons", RobotButtons, queue_size=10)
         pub_robot_analog_values = rospy.Publisher("~analog_values", RobotAnalogValues, queue_size=10)
 
         ids = rospy.get_param('~sensor_ids')
         s_set = rospy.Service('capra_controlpanel/set', Set, handle_controlpanel_set)
+        s_flash = rospy.Service('capra_controlpanel/flash', Flash, handle_controlpanel_flash)
 
         comm.communication.instance = Communication(ids, rospy.get_param('~serial_port'), 19200)
         success = comm.communication.instance.start()
@@ -50,9 +91,8 @@ class ControlPanelServer:
 
         rate = rospy.get_param('~publish_rate')
         r = rospy.Rate(rate)
-        serial_wait = float(rospy.get_param('~serial_wait'))
 
-        rospy.loginfo("Starting controlpanel. Serial polling slep: " + str(serial_wait) + " . Message rate(hz): " + str(r))
+        rospy.loginfo("Starting controlpanel. Message rate(hz): " + str(r))
         while not rospy.is_shutdown():
 
             #Read button data
@@ -62,14 +102,17 @@ class ControlPanelServer:
                 status, reply = comm.communication.instance.send_command("GET " + field)
 
                 if status is False:
-                    rospy.logerr("Error reading '" + field + "': " + reply)
+                    rospy.logerr("Error reading '" + field)
                 else:
+                    reply_value = reply.split(" ")[1]
                     if type(robot_buttons.__getattribute__(field)) is bool:
-                        robot_buttons.__setattr__(field, bool(reply))
+                        if "OFF" in reply:
+                            robot_buttons.__setattr__(field, False)
+                        else:
+                            robot_buttons.__setattr__(field, True)
                     else:
-                        robot_buttons.__setattr__(field, str(reply))
+                        robot_buttons.__setattr__(field, str(reply_value))
 
-                time.sleep(serial_wait)
             rospy.loginfo(robot_buttons)
             pub_robot_buttons.publish(robot_buttons)
 
@@ -80,11 +123,10 @@ class ControlPanelServer:
                 status, reply = comm.communication.instance.send_command("GET " + field)
 
                 if status is False:
-                    rospy.logerr( "Error reading '" + field + "': " + reply)
+                    rospy.logerr( "Error reading '" + field )
                 else:
-                    robot_analog_values.__setattr__(field, float(reply))
+                    robot_analog_values.__setattr__(field, float(reply.split(" ")[1]))
 
-                time.sleep(serial_wait)
             rospy.loginfo(robot_analog_values)
             pub_robot_analog_values.publish(robot_analog_values)
 
